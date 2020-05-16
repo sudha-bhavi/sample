94441c2b1a69979af7c750f87eedd2bb690fdbe1
sudha b <sudhab@softtrends.co.in>
2020-05-16 15:36:14 +0530

    make it better

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using crudtest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using netcore.Models;
+using Newtonsoft.Json;
+
+namespace netcore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            JSONReadWrite readWrite = new JSONReadWrite();
+            people = JsonConvert.DeserializeObject<List<PersonModel>>(readWrite.Read("Person.json"));
+
+            return View(people);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            JSONReadWrite readWrite = new JSONReadWrite();
+            people = JsonConvert.DeserializeObject<List<PersonModel>>(readWrite.Read("Person.json"));
+            PersonModel person = people.FirstOrDefault(x => x.Id == id);
+            return View(person);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(PersonModel personModel)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            JSONReadWrite readWrite = new JSONReadWrite();
+            people = JsonConvert.DeserializeObject<List<PersonModel>>(readWrite.Read("Person.json"));
+
+            PersonModel person = people.FirstOrDefault(x => x.Id == personModel.Id);
+
+            if (person == null)
+            {
+                people.Add(personModel);
+            }
+            else
+            {
+                int index = people.FindIndex(x => x.Id == personModel.Id);
+                people[index] = personModel;
+            }
+
+            string jSONString = JsonConvert.SerializeObject(people);
+            readWrite.Write("Person.json", jSONString);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PersonModel personModel)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            JSONReadWrite readWrite = new JSONReadWrite();
+            people = JsonConvert.DeserializeObject<List<PersonModel>>(readWrite.Read("Person.json"));
+
+            PersonModel person = people.FirstOrDefault(x => x.Id == personModel.Id);
+
+            if (person == null)
+            {
+                people.Add(personModel);
+            }
+            else
+            {
+                int index = people.FindIndex(x => x.Id == personModel.Id);
+                people[index] = personModel;
+            }
+
+            string jSONString = JsonConvert.SerializeObject(people);
+            readWrite.Write("Person.json", jSONString);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            JSONReadWrite readWrite = new JSONReadWrite();
+            people = JsonConvert.DeserializeObject<List<PersonModel>>(readWrite.Read("Person.json"));
+
+            int index = people.FindIndex(x => x.Id == id);
+            people.RemoveAt(index);
+
+            string jSONString = JsonConvert.SerializeObject(people);
+            readWrite.Write("Person.json", jSONString);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+    public class JSONReadWrite
+    {
+        public JSONReadWrite() { }
+
+        public string Read(string fileName)
+        {
+            string root = "wwwroot";
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                //root,
+                //location,
+                fileName);
+
+            string jsonResult;
+
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                jsonResult = streamReader.ReadToEnd();
+            }
+            return jsonResult;
+        }
+
+        public void Write(string fileName, string jSONString)
+        {
+            string root = "wwwroot";
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                //root,
+                //location,
+                fileName);
+
+            using (var streamWriter = File.CreateText(path))
+            {
+                streamWriter.Write(jSONString);
+            }
+        }
+    }
+}
---
+++ Models/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace crudtest.Models
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
